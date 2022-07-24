0be5455acb81f675cbbb230b1c74f36a86c27240
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2022-07-24 14:53:52 +0800

    Added solution for equal sum partition BottomUp.

--- knapsack/knapsack3.go
+++ knapsack/knapsack3.go
@@ -44,3 +44,53 @@ func knapsack3TopDown(arr []int, sum int, n int) bool {
 		return knapsack3TopDown(arr, sum, n+1)
 	}
 }
+
+func TestKnapSack3BottomUP() {
+	arr := []int{6, 9, 7, 8}
+
+	result := knapsack3BottomUp(arr)
+
+	fmt.Println(result)
+}
+
+func knapsack3BottomUp(arr []int) bool {
+
+	sum := 0
+
+	for _, num := range arr {
+		sum += num
+	}
+
+	if sum%2 != 0 {
+		return false
+	}
+
+	sum = sum / 2
+
+	n := len(arr)
+	dp := make([][]bool, len(arr)+1)
+
+	for i := range dp {
+		dp[i] = make([]bool, sum+1)
+		for j := range dp[i] {
+			if i == 0 {
+				dp[i][j] = false
+			}
+			if j == 0 {
+				dp[i][j] = true
+			}
+		}
+	}
+
+	for i := 1; i < n+1; i++ {
+		for j := 1; j < sum+1; j++ {
+			if arr[i-1] <= j {
+				dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[n][sum]
+}
