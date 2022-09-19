c86b2a3553eb2d836c824ceed9c9f6781916e7be
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2022-09-19 21:57:03 +0800

    Added solution for stick cutting problem.

---
+++ knapsack/stickcuttingproblem.go
@@ -0,0 +1,9 @@
+package knapsack
+
+// Solve following problem.
+// https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+
+func TestStickCuttingProblem() {
+
+	// inputs : []int cuts, int n
+}
